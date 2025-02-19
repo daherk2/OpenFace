cd635884b34e205798eaf9874ff6de5c74e05543
Fabio R. D. de Barros <fabio.rosindo.daher.de.barros@gmail.com>
2025-02-19 00:42:50 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,9 +61,9 @@ echo "Essential dependencies installed."
 
 # OpenCV Dependency
 echo "Downloading OpenCV..."
-wget https://github.com/opencv/opencv/archive/4.1.0.zip
-unzip 4.1.0.zip
-cd opencv-4.1.0
+wget https://github.com/opencv/opencv/archive/4.0.1.zip
+unzip 4.0.1.zip
+cd opencv-4.0.1
 mkdir -p build
 cd build
 echo "Installing OpenCV..."
@@ -71,8 +71,8 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB
 make -j4
 sudo make install
 cd ../..
-rm 4.1.0.zip
-sudo rm -r opencv-4.1.0
+rm 4.0.1.zip
+sudo rm -r opencv-4.0.1
 echo "OpenCV installed."
 
 # dlib dependecy
